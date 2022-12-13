c17d06820fed17eea1c3b5a882625646f236ab97
bishalsarkar77730 <bishal77730sarkar@gmail.com>
2022-12-14 01:45:36 +0530

    basic Ui Setup Done

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,25 @@ import React from "react";
 import "./App.css";
 
 // Import Components
-import GenrateQr from "./Components/Qr-Generator/Genrate-Qr";
-import Scanner from "./Components/Qr-Scanner/Scanner";
+// import SignupSignIn from "./Components/Signup-SignIn/SignupSignIn";
+import Home from "./Components/Home/Home";
+// import GenrateQr from "./Components/Qr-Generator/Genrate-Qr";
+// import Scanner from "./Components/Qr-Scanner/Scanner";
 
 const App = () => {
   return (
+    <>
+    <Home />
+    {/* <SignupSignIn />
     <div className="App">
-      <h1>Hello Folks</h1>
       <div className="gen">
         <GenrateQr />
       </div>
       <div className="sca">
         <Scanner />
       </div>
-    </div>
+    </div> */}
+    </>
   );
 };
 
